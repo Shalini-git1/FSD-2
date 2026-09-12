f789c1a5fdf40895cd8237bd081e50074461f37c
Shalini Marampudi <shalinimarampudi1508@gmail.com>
2026-09-12 10:41:06 +0530

    Delete week-8/middleware.js

--- week-8/middleware.js
+++
@@ -1,7 +0,0 @@
-app.set('view engine', 'ejs'); 
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
-app.get('/', async (req, res) => {
-    const students = await Student.find();
-    res.render('apphome', { students });
-});
